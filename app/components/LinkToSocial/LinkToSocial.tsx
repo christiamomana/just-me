0ac935c9ffd9a4d5cc90e69c68cde591ce23b70b
Cristiam Omana <christiamchivico@gmail.com>
2024-01-23 23:25:36 +0000

    fix type icon

--- app/components/LinkToSocial/LinkToSocial.tsx
+++ app/components/LinkToSocial/LinkToSocial.tsx
@@ -1,10 +1,6 @@
-import { SvgIconProps } from "@material-ui/core/SvgIcon";
+import { SvgIconComponent } from "@mui/icons-material";
 
-export const LinkToSocial = ({
-  Icon,
-}: {
-  Icon: React.ComponentType<SvgIconProps>;
-}) => {
+export const LinkToSocial = ({ Icon }: { Icon: SvgIconComponent }) => {
   return (
     <a
       className="justify-center rounded-xl text-sm font-semibold py-3 px-4 bg-slate-900 text-green-400 hover:bg-slate-700"
